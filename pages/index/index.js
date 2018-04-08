b4335999176b4ee6af291ced8b65c7bcc0ac7dd3
javion25 <228806684@qq.com>
2018-04-08 20:29:18 +0800

    新增获取热门歌曲数据以及页面渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    hotSongList: []
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,39 +13,18 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    // 获取热门歌曲
+    this.getHotSongList()
+  },
+  getHotSongList: function(e) {
+    let _this = this;
+    wx.request({
+      url: app.globalData.serverUrl + '/song?limit=20',
+      success: function(res) {
+        _this.setData({
+          hotSongList: res.data.data
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
 })
