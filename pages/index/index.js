269fd9e220793dd61181d7459e23a68b22e8feaa
javion25 <228806684@qq.com>
2018-04-12 18:43:33 +0800

    add feedback model

--- pages/index/index.js
+++ pages/index/index.js
@@ -23,6 +23,12 @@ Page({
   onLoad: function () {
     this.getSongList()
   },
+  // input数据绑定
+  bindValueInput: function (e) {
+    this.setData({
+      [e.target.dataset.name]: e.detail.value
+    })
+  },
   getSongList: function(e) {
     let _this = this
     let reqUrl
@@ -48,23 +54,52 @@ Page({
       }
     })
   },
-  bindKeywordInput: function(e) {
-    this.setData({
-      keyword: e.detail.value
-    })
-  },
   changeFeedbackModal: function(e) {
     if (!e.target.dataset.hasOwnProperty('isShow')) return
     this.setData({
       isShowModal: !!+e.target.dataset.isShow
     })
   },
-  bindValueInput: function(e) {
-    this.setData({
-      [e.target.dataset.name]: e.detail.value
-    })
-  },
+  // 提交反馈
   submitFeedback: function(e) {
-    // 反馈接口
+    if (this.data.suggest.length === 0) {
+      wx.showToast({
+        title: '反馈内容不能为空！',
+        icon: 'none',
+        duration: 2000
+      })
+      return
+    }
+    let _this = this
+    let reqUrl = app.globalData.serverUrl + '/feedback'
+    let data = {
+      contact: this.data.contact,
+      suggest: this.data.suggest
+    }
+    wx.request({
+      url: reqUrl,
+      data: data,
+      method: 'POST',
+      success: function (res) {
+        if (res.data.code === 200) {
+          wx.showToast({
+            title: '提交成功',
+            icon: 'success',
+            duration: 2000,
+            success: function () {
+              _this.setData({
+                isShowModal: false
+              })
+            }
+          })
+        } else {
+          wx.showToast({
+            title: res.data.error,
+            icon: 'none',
+            duration: 2000
+          })
+        }
+      }
+    })
   }
 })
