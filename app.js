44c3c9991d5cc51eda13b0c069c10bccc01440a3
javion25 <228806684@qq.com>
2018-06-25 12:47:13 +0800

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
   onLaunch: function () {
   },
   globalData: {
-    // serverUrl: 'https://wx.javion.cn/api'
-    serverUrl: 'http://localhost:5000/api'
+    serverUrl: 'https://wx.javion.cn/api'
+    // serverUrl: 'http://localhost:5000/api'
   }
 })
