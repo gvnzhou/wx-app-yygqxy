ed595ad887c54647a16e32b3165c76ec086750e2
javion25 <228806684@qq.com>
2018-06-27 17:41:28 +0800

    1.修复提交反馈一直提示'反馈内容不能为空' 2.新增无搜索结果时文字提示 3.新增点击历史搜索后填充keyword到输入框 4.封装反馈按钮组件 5.util工具类新增'bindValueInput'方法

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 //app.js
-// TODO: 1.优化无搜索结果展示 2.优化搜索加载中 3.输入完成后立即搜索
-
 App({
   onLaunch: function () {
   },
   globalData: {
-    serverUrl: 'https://yygqxy.javion.cn/api'
+    serverUrl: 'https://yygqxy.javion.cn/api',
     // serverUrl: 'http://localhost:5000/api'
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,15 +5,7 @@ Page({
   data: {
     isShowModal: false,
     keyword: '',
-    songList: [],
-    suggest: '',
-    contact: ''
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    songList: []
   },
   onLoad: function () {
     this.getSongList()
@@ -33,54 +25,6 @@ Page({
         _this.setData({
           songList: res.data.data
         })
-      }
-    })
-  },
-  changeFeedbackModal: function(e) {
-    if (!e.target.dataset.hasOwnProperty('isShow')) return
-    this.setData({
-      isShowModal: !!+e.target.dataset.isShow
-    })
-  },
-  // 提交反馈
-  submitFeedback: function(e) {
-    if (this.data.suggest.length === 0) {
-      wx.showToast({
-        title: '反馈内容不能为空！',
-        icon: 'none',
-        duration: 2000
-      })
-      return
-    }
-    let _this = this
-    let reqUrl = app.globalData.serverUrl + '/feedback'
-    let data = {
-      contact: this.data.contact,
-      suggest: this.data.suggest
-    }
-    wx.request({
-      url: reqUrl,
-      data: data,
-      method: 'POST',
-      success: function (res) {
-        if (res.data.code === 200) {
-          wx.showToast({
-            title: '提交成功',
-            icon: 'success',
-            duration: 2000,
-            success: function () {
-              _this.setData({
-                isShowModal: false
-              })
-            }
-          })
-        } else {
-          wx.showToast({
-            title: res.data.error,
-            icon: 'none',
-            duration: 2000
-          })
-        }
       }
     })
   }
